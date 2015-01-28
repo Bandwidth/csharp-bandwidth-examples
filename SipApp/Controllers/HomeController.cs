98cd9be6343601d8e4887514105fcd182fb59169
Andrey Belchikov <belchikovs@gmail.com>
2015-01-28 17:54:30 +0300

    Fixed found issues

--- SipApp/Controllers/HomeController.cs
+++ SipApp/Controllers/HomeController.cs
@@ -24,13 +24,13 @@ namespace SipApp.Controllers
                            {
                                {"name", Common.UserName}, 
                                {"description", Common.UserName + " mobile client"},
-                               {"application_id", app.Id},
-                               {"domain_id", domain.Id},
+                               {"applicationId", app.Id},
+                               {"domainId", domain.Id},
                                {"enabled", true},
                                {"credentials", new Dictionary<string, object>{{"password", "1234567890"}}}
                            }));
             return Content(string.Format("This app is ready to use<br/>Please configure your sip phone with account <b>{0}@{1}.bwapp.bwsip.io</b> and password <b>1234567890</b>." +
-                " Please check if your sip client is online. Then press this button.<br/><form action=\"{3}\"><input type=\"submit\">Call me</input></form>", Common.UserName, Common.DomainName, Url.Action("CallToSip", "Home")));
+                " Please check if your sip client is online. Then press this button.<br/><form action=\"{2}\"><input type=\"submit\" value=\"Call me\"></input></form>", Common.UserName, Common.DomainName, Url.Action("CallToSip", "Home")));
         }
 
         // POST: Call
